7ff6cabf90c9b52ffcb5eb6d1b321d885047767b
Dariusz Serwaczak <serwaczak.dariusz@gmail.com>
2023-12-28 22:19:01 +0100

    Zadanie dzialalo wczesniej na MacOS, dodaje kompatybilnosc z Windows-em

--- src/Assignments/Uniforms/shaders/base_fs.glsl
+++ src/Assignments/Uniforms/shaders/base_fs.glsl
@@ -4,10 +4,10 @@ in vec3 vertexColor;
 
 out vec4 FragColor;
 
-#if defined(__APPLE__)
-    layout(std140, binding=1) uniform Modifier
-#else
+#if __APPLE__
     layout(std140) uniform Modifier
+#else
+    layout(std140, binding=0) uniform Modifier
 #endif
 {
  float strength;
